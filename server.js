ad8b012e83568a5b015bc48f84a7ad795c1fcb3e
Jordan S <jpstanway@gmail.com>
2019-03-02 11:02:24 -0800

    set static assets on server for production and create post build script

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const helmet = require("helmet");
+const path = require("path");
 
 const mongoose = require("mongoose");
 const db = require("./config/keys").MONGO_URI;
@@ -35,6 +36,16 @@ app.use("/api/users", users);
 app.use("/api/profiles", profiles);
 app.use("/api/posts", posts);
 app.use("/api/inboxes", inboxes);
+
+// serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // server setup
 const port = process.env.PORT || 5000;
