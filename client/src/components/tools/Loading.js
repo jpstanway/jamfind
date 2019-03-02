eb92defca6a3490d88b954aac69bd7f76f466b67
Jordan S <jpstanway@gmail.com>
2019-03-02 11:28:29 -0800

    move all images to cloud service

--- client/src/components/tools/Loading.js
+++ client/src/components/tools/Loading.js
@@ -1,12 +1,14 @@
 import React from "react";
-import loading from "../../imgs/loading.gif";
 
 const Loading = () => {
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-12 text-center">
-          <img src={loading} alt="Loading..." />
+          <img
+            src="https://res.cloudinary.com/mtninja/image/upload/v1551554469/loading_yunyf4.gif"
+            alt="Loading..."
+          />
         </div>
       </div>
     </div>
